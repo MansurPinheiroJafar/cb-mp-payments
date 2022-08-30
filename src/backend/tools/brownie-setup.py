05f62b20769e0002b4ddfe068b9fca79378a6117
MansurPinheiroJafar <mansur.pinheiro.jafar@gmail.com>
2022-08-30 21:24:39 +0100

    Adding brownie setup tool and tests files for tools

---
+++ src/backend/tools/brownie-setup.py
@@ -0,0 +1,17 @@
+import os
+
+def is_brownie_installed() -> bool:
+    """
+        Checks if brownie is installed
+        :returns True if a brownie installation is present otherwise False
+        :raises ...
+    """
+    pass
+
+def install_brownie() -> bool:
+    """
+        Installs brownie.
+        :returns True if the installation is successful.
+        raises ...
+    """
+    pass
