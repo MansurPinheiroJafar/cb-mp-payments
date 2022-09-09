47a7bb4a1de11525e1aea962a8ed15e462f0d4a1
MansurPinheiroJafar <mansur.pinheiro.jafar@gmail.com>
2022-09-10 00:45:49 +0100

    baked token and updated requirements.txt

---
+++ Notes.py
@@ -0,0 +1,12 @@
+"""
+    Requires node, npm and the ganache cli as brownie has a dependency on it
+
+    npm install -g ganache-cli
+    
+    brownie bake token -> created the base project
+    brownie console -> initialise the console
+    dir() -> list of objects
+    help(<object-name-from-dir-output>) -> returns docstring for each object
+
+
+"""
